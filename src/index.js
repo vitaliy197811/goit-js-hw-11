28f3b5e30ee75a62cd913ac9b22220fe9f0fce83
Vitaliy <103895059+vitaliy197811@users.noreply.github.com>
2022-10-06 21:34:58 +0300

    start1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
+const axios = require('axios').default;
+
+// axios.post('/user', {
+//         firstName: 'Fred',
+//         lastName: 'Flintstone'
+//         })
+//     .then(function (response) {
+//         console.log(response);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+// });
